e19b2e91d8b2b42f06ece7f4d03e4c312122e070
PBrocks <paul@barthmaier.rocks>
2018-02-19 09:38:43 -0500

    Enhancement: Schemeless URLs
    Schemeless, or really, protocol-less, URLs are recommended so the client gets to determine whether SSL is possible.

--- pmpro-shipping.php
+++ pmpro-shipping.php
@@ -758,8 +758,8 @@ add_action( 'wp_enqueue_scripts', 'pmproship_load_js' );
 function pmproship_plugin_row_meta( $links, $file ) {
 	if ( strpos( $file, 'pmpro-shipping.php' ) !== false ) {
 		$new_links = array(
-			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/add-ons/shipping-address-membership-checkout/' ) . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
-			'<a href="' . esc_url( 'https://paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url( '//www.paidmembershipspro.com/add-ons/shipping-address-membership-checkout/' ) . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url( '//paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
 		);
 		$links     = array_merge( $links, $new_links );
 	}
